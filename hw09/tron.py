3205ce588d6ab68eff8ce1e79725079c03bbb0bd
Alec Goebel <agoebel@swordandspade.com>
2012-02-23 18:07:26 -0500

    hw09 posted

--- hw09/tron.py
+++ hw09/tron.py
@@ -4,3 +4,6 @@ tron.py
 
 The simple game of tron with two players.  Press the space bar to start the game.  Player 1 (red) is controlled with WSAD and player 2 (blue) is controlled with the arrow keys.  Once the game is over, press space to reset and then again to restart.  Escape quits the program.
 """
+
+import pygame
+from pygame.locals import *
