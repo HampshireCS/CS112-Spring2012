890b50514ab9d1f9d641096b831af42526ba4cd0
Alec Goebel <agoebel@swordandspade.com>
2012-04-26 19:37:36 -0400

    fixed coin color

--- samplecode/networking_example/coinget/client/sprites/coin.py
+++ samplecode/networking_example/coinget/client/sprites/coin.py
@@ -3,9 +3,9 @@ from louie import dispatcher
 import pygame
 from pygame.sprite import Sprite, Group
 
+from coinget import settings
 from coinget import signals
 from coinget.world import Coin
-
 
 class CoinSprite(Sprite):
     def __init__(self, coin):
@@ -14,13 +14,16 @@ class CoinSprite(Sprite):
         self.image = pygame.Surface(coin.rect.size)
         self.rect = self.image.get_rect()
 
-        self.image.fill((0,0,0))
-        self.image.fill((255,255,0), self.rect.inflate(-4,-4))
-
         self.rect.center = coin.rect.center
 
         self.coin = coin
         dispatcher.connect(self.kill, signal=signals.DEL_OBJECT, sender=coin)
+    def redraw(self):
+        rect = self.image.get_rect()
+        ratio = 1.0 * self.coin.life / settings.COIN_LIFE
+
+        self.image.fill((0,0,0))
+        self.image.fill((255,255 * ratio,0), rect.inflate(-4,-4))
 
 
 class CoinGroup(Group):
